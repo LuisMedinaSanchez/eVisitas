ba321f4c3ca5beab16b08759de01a90dcd8f5614
Luis Medina Sánchez <luismedinasanchez@hotmail.com>
2018-09-30 21:51:38 -0500

    Areglar la ruta: RUTA_LOGING. Por:			 RUTA_LOGIN.

--- app/config.inc.php
+++ app/config.inc.php
@@ -6,4 +6,4 @@ define('NOMBRE_BD', 'blog');
 
 define("SERVIDOR", "http://192.168.1.1:88/transpheric");
 define("REGISTRO_CORRECTO", SERVIDOR."/registro-correcto.php");
-define("RUTA_LOGING", SERVIDOR."/loging.php");
+define("RUTA_LOGIN", SERVIDOR."/login.php");
