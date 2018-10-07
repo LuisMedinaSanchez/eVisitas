07019bfc87138134f79008dd1627ce0c321e5436
Luis Medina Sánchez <luismedinasanchez@hotmail.com>
2018-10-06 19:29:21 -0500

    Probando cambios en la direccion URL

--- app/config.inc.php
+++ app/config.inc.php
@@ -4,11 +4,11 @@ define('NOMBRE_USUARIO', 'root');
 define('PASSWORD', '');
 define('NOMBRE_BD', 'blog');
 
-define("SERVIDOR", "http://192.168.1.1:88/transpheric");
-define("REGISTRO_CORRECTO", SERVIDOR."/registro-correcto.php");
-define("RUTA_LOGIN", SERVIDOR."/login.php");
-define("RUTA_LOGIN_CIERRE", SERVIDOR."/SesionCerrada.php");
-define("RUTA_SESION_INICIADA", SERVIDOR."/dashboard.php");
+define("SERVIDOR", "http://localhost:88/transpheric");
+define("REGISTRO_CORRECTO", SERVIDOR."/registro-correcto");
+define("RUTA_LOGIN", SERVIDOR."/login");
+define("RUTA_LOGIN_CIERRE", SERVIDOR."/SesionCerrada");
+define("RUTA_SESION_INICIADA", SERVIDOR."/dashboard");
 define("RUTA_REGISTRO_VISITAS", SERVIDOR."/registro_visitas.php");
-define("RUTA_SALIDA_VISITAS", SERVIDOR."/salida_visitas.php");
-define("RUTA_HISTORICO_VISITAS", SERVIDOR."/historico_visitas.php");
+define("RUTA_SALIDA_VISITAS", SERVIDOR."/salida_visitas");
+define("RUTA_HISTORICO_VISITAS", SERVIDOR."/historico_visitas");
